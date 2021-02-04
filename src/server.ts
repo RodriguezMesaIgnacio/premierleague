3e57a0bc30ae577fe659111bcc2c4c8f107a838e
Ignacio <ignacionrmesa19>
2021-02-04 12:17:12 +0100

    .

--- src/server.ts
+++ src/server.ts
@@ -21,8 +21,8 @@ class Server {
         this.app.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", '*');
             res.header("Access-Control-Allow-Credentials", 'true');
-            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-            res.header("Access-Control-Allow-Headers", '*');
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE');
+            res.header("Access-Control-Allow-Headers", 'Content-Type');
             next();
         });
 
