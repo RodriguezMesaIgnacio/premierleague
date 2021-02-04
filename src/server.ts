a5e8fdd1b28154b74257b7c9147a84ccfb7bd6e4
Ignacio <ignacionrmesa19>
2021-02-04 12:19:57 +0100

    .

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ class Server {
             res.header("Access-Control-Allow-Origin", '*');
             res.header("Access-Control-Allow-Credentials", 'true');
             res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE');
-            res.header("Access-Control-Allow-Headers", 'Content-Type');
+            res.header("Access-Control-Allow-Headers", 'Origin');
             next();
         });
 
